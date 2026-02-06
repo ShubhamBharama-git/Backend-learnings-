370f5d42d0021bdc1e27159f9ef4dd47de23e1a6
user.name:"Shubham Bharama <shubhambharama18@gmail.com>
2026-02-06 13:37:20 +0530

    Initial backend learning commit

---
+++ Nodejs/index.js
@@ -0,0 +1,21 @@
+// Nodejs -> 
+// - js runtime env
+// - runs js code  outside the browser
+// - this is built on chromes v8 engine 
+
+// Packages -> reusable peace of code 
+// module -> built in packages from node js 
+
+// servers ✅
+
+const http = require("http");
+
+// create server 
+const server = http.createServer((req, res) => {
+    res.end("Port is runing on server!");
+});
+
+// start server 
+server.listen(3000, () => {
+    console.log("server is runing on port : 3000");
+})
